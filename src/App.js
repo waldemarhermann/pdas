83526da06cd6da9a0c2083700ae94408e0126f75
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-08 11:45:40 +0100

    add component and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className='hero is-primary'>
+        <div className='hero-body'>
+          <p className='title'>Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className='container'>
+        <div className='section'>
+          <div className='columns'>
+            <div className='column is-3'>
+              <ProfileCard 
+              image={AlexaImage} 
+              title='Alexa' 
+              handle='@alexa99' 
+              description='Alexa was created by Amazon and helps you to buy things.'
+              />
+            </div>
+            <div className='column is-3'>
+              <ProfileCard 
+              image={CortanaImage} 
+              title='Cortana' 
+              handle='@cortana32' 
+              description='Cortana was created by Microsoft and helps you to find things.'
+              />
+            </div>
+            <div className='column is-3'>
+              <ProfileCard 
+              image={SiriImage} 
+              title='Siri' 
+              handle='@siri01' 
+              description='Siri was created by Apple and helps you to make your life easier.'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
